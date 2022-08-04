e696995c4a3c98fc174d7f5f79c6cc8dbdc2b8f2
Cloud User <ec2-user@ip-172-31-45-205.ap-south-1.compute.internal>
2022-08-04 13:48:58 +0000

    commit to github

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main() {
+
+        biggest();
+        factorial();
+        reverse();
+        pallindrome();
+        biggest3();
+        addnumber();
+        fibonacci();
+        sorting();
+}
+
